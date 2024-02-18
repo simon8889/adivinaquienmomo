445ad32491b52fade8786ac57b5ee1f2eaa8d302
simon <simondjv2016@gmail.com>
2024-02-17 22:13:01 -0500

    beuty changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import './App.css'
 import Card from './components/Card'
 
-function App() {
+const App = () => {
     return (
         <div className="app">
-            <h1>Adivina el personaje de Momo</h1>
+            <h1>Adivina el personaje de Momo 🕰️🐢⏳</h1>
             <p>Presta atención a las pistas, descarta personajes y adivina el personaje.</p>
-            <br/>
             <div className='app_container'>
-                <Card name="Momo" />
-                <Card name="Beppo Barrendero" />
-                <Card name="Gigi Cicerones" />
-                <Card name="Blanco" />
-                <Card name="Hombres Grises" />
-                <Card name="Paolo" />
-                <Card name="Señor Fusi" />
-                <Card name="Maria" />
-                <Card name="Massimo" />
-                <Card name="Nicola" />
-                <Card name="Nino" />
+                <Card name="Momo" emoji="👧" />
+                <Card name="Beppo Barrendero" emoji="🧹"/>
+                <Card name="Gigi Cicerones" emoji="📖️"/>
+                <Card name="Massimo" emoji="👦🏾" />
+                <Card name="Hombres Grises" emoji="👤"/>
+                <Card name="Paolo" emoji="👦🏻"/>
+                <Card name="Señor Fusi" emoji="💈"/>
+                <Card name="Maria" emoji="👧🏻"/>
+                <Card name="Blanco" emoji="👦🏼"/>
+                <Card name="Nicola" emoji="🛠️"/>
+                <Card name="Nino" emoji="🏨"/>
             </div>
         </div>
     );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,13 @@
 import { React, useState } from 'react'
 import "./Card.css"
 
-const Card = ({ name }) => {
+const Card = ({ name, emoji }) => {
     
     const [ out, setOut ] = useState(false)
 
     return (
         <div className={ out ? "discard" : "card" } onClick={() => setOut(!out)}>
-            <p>{ out ? "" : name }</p>
+            <p>{ out ? emoji : name }</p>
         </div>
     )
 }
